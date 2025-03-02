f447765c616f303ae2302959c75ccc2dd78f68fa
Oluwatomisin780 <tomisinoyediran@gmail.com>
2025-03-02 13:26:33 +0100

    edumaro_inteview_prep

--- src/modules/subject/subject.module.ts
+++ src/modules/subject/subject.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SubjectService } from './subject.service';
 import { SubjectController } from './subject.controller';
 import { PrismaModule } from '../../services/prisma/prisma.module';
+import { JwtStrategy } from '../../common/strategies/jwt-auth.strategy';
 
 @Module({
   imports: [PrismaModule],
-  providers: [SubjectService],
+  providers: [SubjectService, JwtStrategy],
   controllers: [SubjectController],
 })
 export class SubjectModule {}
